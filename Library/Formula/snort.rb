336f1e75fee9369dc9bf9d45d16c8ae93696c56c
Dave Turnbull <dave@redcordial.org>
2011-07-11 11:32:20 +1000

    update snort brew with more options

--- Library/Formula/snort.rb
+++ Library/Formula/snort.rb
@@ -9,8 +9,28 @@ class Snort < Formula
   depends_on 'libdaq'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}", "--enable-ipv6"
+    system "./configure",
+           "--prefix=#{prefix}",
+           "--enable-ipv6",
+           "--enable-zlib",
+           "--enable-gre",
+           "--enable-mpls",
+           "--enable-targetbased",
+           "--enable-decoder-preprocessor-rules",
+           "--enable-ppm",
+           "--enable-perfprofiling",
+           "--enable-inline-init-failopen",
+           "--enable-pthread",
+           "--enable-ppm-test",
+           "--enable-sourcefire",
+           "--enable-active-response",
+           "--enable-normalizer",
+           "--enable-reload",
+           "--enable-reload-error-restart",
+           "--enable-react",
+           "--enable-flexresp3",
+           "--enable-aruba"
+
     system "make install"
   end
 end
